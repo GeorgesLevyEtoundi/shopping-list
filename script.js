54e05de6bb8f30abdd233560d9277831a0246e86
GeorgesLevyEtoundi <etoundioyono.georeslevy@gmail.com>
2024-04-15 06:56:17 +0200

    Add filter feature to shopping list

--- script.js
+++ script.js
@@ -1,52 +1,130 @@
-const itemForm = document.getElementById('item-form')
-const itemInput = document.getElementById('item-input')
-const itemList  = document.getElementById('item-list')
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
+// add an item
 function addItem(e) {
-  e.preventDefault()
+	e.preventDefault();
 
-  const newItem = itemInput.value
-  
-  if (newItem === '') {
-    alert('Please add an item')
+	const newItem = itemInput.value;
 
-    return
-  }
+	if (newItem === '') {
+		alert('Please enter the name of an item first');
 
-  // create list item
-  const li = document.createElement('li')
+		return;
+	}
 
-  li.appendChild(document.createTextNode(newItem))
+	// check it exists
+	if (!checkItemExists(newItem)) {
+		// create list item
+		const li = document.createElement('li');
 
-  const button = createButton('remove-item btn-link text-red')
+		li.appendChild(document.createTextNode(newItem));
 
-  li.appendChild(button)
+		const button = createButton('remove-item btn-link text-red');
 
-  itemList.appendChild(li)
+		li.appendChild(button);
 
-  itemInput.value = ''
+		// add item to the DOM
+		itemList.appendChild(li);
+
+		checkUI();
+
+		itemInput.value = '';
+	}
 }
 
 function createButton(classes) {
-  const button = document.createElement('button')
+	const button = document.createElement('button');
 
-  button.className = classes
+	button.className = classes;
 
-  const icon = createIcon('fa-solid fa-xmark')
+	const icon = createIcon('fa-solid fa-xmark');
 
-  button.appendChild(icon)
+	button.appendChild(icon);
 
-  return button
+	return button;
 }
 
 function createIcon(classes) {
+	const icon = document.createElement('i');
 
-  const icon = document.createElement('i')
+	icon.className = classes;
 
-  icon.className = classes
+	return icon;
+}
 
-  return icon
+// remove an item
+function removeItem(e) {
+	if (e.target.parentElement.classList.contains('remove-item')) {
+		if (confirm('Are you sure?')) {
+			e.target.parentElement.parentElement.remove();
+		}
+
+		// check if the list item is empty after an item was removed
+		checkUI();
+	}
+}
+
+// clear all items
+function clearItems() {
+	while (itemList.firstChild) {
+		itemList.removeChild(itemList.firstChild);
+	}
+
+	checkUI();
+}
+
+function filterItems(e) {
+	const items = itemList.querySelectorAll('li');
+	const text = e.target.value.toLowerCase();
+
+	for (let item of items) {
+		const itemValue = item.firstChild.textContent.toLocaleLowerCase();
+
+		if (itemValue.indexOf(text) != -1) {
+			item.style.display = 'flex';
+		} else {
+			item.style.display = 'none';
+		}
+	}
+}
+
+// check if the list item is empty
+function checkUI() {
+	const items = itemList.querySelectorAll('li');
+
+	if (items.length === 0) {
+		itemFilter.style.display = 'none';
+		clearBtn.style.display = 'none';
+	} else {
+		itemFilter.style.display = 'block';
+		clearBtn.style.display = 'block';
+	}
+}
+
+// check item already in the list
+function checkItemExists(itemName) {
+	const items = itemList.querySelectorAll('li');
+
+	for (const item of items) {
+		if (item.textContent === itemName) {
+			alert(
+				'The item you are trying to add is already in the list. Please consider changing the name.'
+			);
+
+			return true;
+		}
+	}
 }
 
 // event listeners
-itemForm.addEventListener('submit', addItem)
+itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
+
+// on page load
+checkUI();
